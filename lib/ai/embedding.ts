e649860b8823f075c87ef3b88c3836387c4c0016
ShreyDhyani <shrey@betalectic.com>
2025-09-22 15:02:53 +0530

    chore: formatting

--- lib/ai/embedding.ts
+++ lib/ai/embedding.ts
@@ -59,15 +59,18 @@ export const generateEmbedding = async (value: string): Promise<number[]> => {
 
 export const findRelevantContent = async (userQuery: string) => {
   const userQueryEmbedded = await generateEmbedding(userQuery);
+
   const similarity = sql<number>`1 - (${cosineDistance(
     embeddings.embedding,
     userQueryEmbedded
   )})`;
+
   const similarGuides = await db
     .select({ name: embeddings.content, similarity })
     .from(embeddings)
     .where(gt(similarity, 0.5))
     .orderBy((t) => desc(t.similarity))
     .limit(4);
+
   return similarGuides;
 };
